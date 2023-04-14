a27922ec9a72328b5a13b692dd3d45051521af3c
Daniel McBride <danielmcbride@Daniels-MacBook-Pro.local>
2023-04-14 18:07:18 +0100

    Basic first splash page

--- aridni-orca-vue/src/App.vue
+++ aridni-orca-vue/src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <video autoplay muted loop id="background-video">
+      <source src="./assets/videos/background-video.mp4" type="video/mp4">
+    </video>
+    <img src="./assets/images/logo-pink.png" id="welcome-logo">
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -23,4 +22,32 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#background-video {
+  top: 0;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+}
+
+#welcome-logo {
+  margin: auto;
+  cursor: url(./assets/images/mushroom-emoji-text-cursor.png) 10 6, move;
+}
+
+@media (orientation: landscape) {
+  #welcome-logo {
+    height: 85vh;
+  }
+}
+
+@media (orientation: portrait) {
+  #welcome-logo {
+    width: 70vw;
+  }
+}
+
 </style>
