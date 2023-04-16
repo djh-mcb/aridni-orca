203e357a05d3a099fa3f4d9f06cd0adf3a4e6365
Daniel McBride <danielmcbride@Daniels-MBP.fritz.box>
2023-04-16 19:24:44 +0100

    Add social media bar to welcome page

--- aridni-orca-vue/src/views/WelcomePage.vue
+++ aridni-orca-vue/src/views/WelcomePage.vue
@@ -1,23 +1,33 @@
 <template>
     <WelcomeLogo @click="goToHome"/>
+    <SocialsBar className="socialsBar"/>
 </template>
 
 <script>
 import WelcomeLogo from '../components/WelcomeLogo.vue'
+import SocialsBar from '../components/SocialsBar.vue'
 
 export default {
     name: 'WelcomePage',
     components: {
-        WelcomeLogo
+        WelcomeLogo,
+        SocialsBar
     },
     methods: {
-    goToHome() {
-        this.$router.push({ name: "home" });
+        goToHome() {
+            this.$router.push({ name: "home" });
+        }
     }
-  }
 }
 </script>
 
 <style>
+.socialsBar {
+    position: absolute;
+    top: calc(100% - 3.5em);
+    left: 1em;
+    height: 2.5em;
+    width: 12em;
+}
 </style>
   
