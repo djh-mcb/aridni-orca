52059f8c2ebc5449245719758252e573dfed6cb7
Daniel McBride <danielmcbride@Daniels-MBP.fritz.box>
2023-04-26 12:52:30 +0100

    Fix ListenPane spotify ad positioning issue

--- src/components/ListenPane.vue
+++ src/components/ListenPane.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="listenPaneBox">
     <NavigationPane class="listenPane" source="listen-image.gif" accentColor="blue" text="listen" @click="showPlayer = true"/>
-    <iframe v-show=showPlayer class="embeddedPlayer" src="https://open.spotify.com/embed/artist/0H4Y63FfweRZmLlIohWamc?utm_source=generator&theme=1" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <div v-show=showPlayer class="embeddedPlayerBackground"></div>
+    <iframe @click="showPlayer = false" v-show=showPlayer class="embeddedPlayer" src="https://open.spotify.com/embed/artist/0H4Y63FfweRZmLlIohWamc?utm_source=generator&theme=1" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     <ImagePane v-show=showPlayer class="returnToVideoButton clickable" source="listen-image.gif" accentColor="blue" @click="showPlayer = false"/>
   </div>
 </template>
@@ -34,19 +35,27 @@ export default {
   height: 100%;
 }
 
+.embeddedPlayerBackground {
+  position: absolute;
+  top: calc(var(--border-size) - 1px);
+  left: calc(var(--border-size) - 1px);
+  height: calc(100% - var(--border-size) * 2 + 2px);
+  width: calc(100% - var(--border-size) * 2 + 2px);
+  background-color: #C11741
+}
+
 .embeddedPlayer {
     position: absolute;
     top: calc(var(--border-size) - 1px);
     left: calc(var(--border-size) - 1px);
-    height: calc(100% - var(--border-size) * 2 + 2px);
+    height: 80%;
     width: calc(100% - var(--border-size) * 2 + 2px);
-    background-color: blue;
 }
 
 .returnToVideoButton {
   position: absolute;
   width: 20%;
-  top: calc(80% + var(--border-size) - 1px);
+  top: calc(80% + var(--border-size) - 2px);
   left: 0;
 }
 </style>
