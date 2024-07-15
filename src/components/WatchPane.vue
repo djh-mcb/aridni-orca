f20c7f36178bccbde1060d19a2c04b136cf026e1
Daniel McBride <dmcbride@inmusicbrands.com>
2024-07-15 16:36:21 +0100

    Update watch pane to The Palace

--- src/components/WatchPane.vue
+++ src/components/WatchPane.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="watchPaneBox">
     <NavigationPane class="watchPane" source="watch-image.webp" accentColor=pink text="watch" @click="showPlayerAndPauseMusic"/>
-    <iframe v-show=showPlayer class="embeddedPlayer" src="https://www.youtube.com/embed/TD0qelgA4aQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe v-show=showPlayer class="embeddedPlayer" src="https://www.youtube.com/embed/bGePQWRfElg?si=-dctx__3OakPs7WU" title="The Palace" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   </div>
 </template>
 
